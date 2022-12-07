57d184510b7831c141998b1f263f49ddcfc0fd2b
Grzegorz Jureczko <jureczko.grzegorz@gmail.com>
2022-12-07 08:15:01 +0100

    added user registration

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 
 from . import views
+from . import views
 
 app_name = 'users'
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
     
 ]
