f7f67fc4216bf7bc7ce3a97a65e2179f85c55288
Grzegorz Jureczko <jureczko.grzegorz@gmail.com>
2023-03-06 22:47:46 +0100

    book add validation

--- static/js/main.js
+++ static/js/main.js
@@ -4,17 +4,50 @@ if (window.location.pathname === '/dashboard/') {
         let bookAddButton = document.getElementById(`add_btn_${comp}`)
         let bookAddComp = document.getElementById(`add_comp_${comp}`)
         bookAddButton.addEventListener('click', () => {
+            if (bookAddComp.classList.contains('d-none') === false) {
+                bookAddComp.classList.toggle('d-none')
+            } else {
 
-            // disable all previously opened submit forms
-            for (let compDisable = 1; compDisable < booksLen; compDisable++) {
-                let bookAddCompDisable = document.getElementById(`add_comp_${compDisable}`)
-                if (bookAddCompDisable.classList.contains('d-none') === false) {
-                    bookAddCompDisable.classList.toggle('d-none')
+                // disable all previously opened submit forms and error msg
+                for (let compDisable = 1; compDisable < booksLen; compDisable++) {
+                    let bookAddCompDisable = document.getElementById(`add_comp_${compDisable}`)
+                    let errMsgDisable = document.getElementById(`err_msg_${comp}`)
+                    if (bookAddCompDisable.classList.contains('d-none') === false) {
+                        bookAddCompDisable.classList.toggle('d-none')
+                    }
+                    if (errMsgDisable.classList.contains('d-none') === false) {
+                        errMsgDisable.classList.toggle(('d-none'))
+                    }
                 }
+                // open only one submit form at a time
+                bookAddComp.classList.toggle('d-none')
             }
-            // open only one submit form at a time
-            bookAddComp.classList.toggle('d-none')
+
         })
+
+        let submitButton = document.getElementById(`submit_button_${comp}`)
+
+        // show error msg
+        submitButton.addEventListener('click', e => {
+            let bookInput = document.getElementById(`input_${comp}`).value
+            let errMsg = document.getElementById(`err_msg_${comp}`)
+            if (bookInput === "") {
+                errMsg.classList.toggle('d-none')
+                e.preventDefault()
+            }
+
+
+        })
+
+        // let bookSegment = document.getElementById(`book_segment_${comp}`)
+        // let submitButton = document.getElementById(`submit_button_${comp}`)
+        // let addInfo = document.getElementById(`add-info_${comp}`)
+        //
+        // submitButton.addEventListener('click', () => {
+        //     bookSegment.classList.add('d-none')
+        //     bookAddComp.classList.toggle('d-none')
+        //     addInfo.classList.toggle('d-none')
+        // })
     }
 }
 
