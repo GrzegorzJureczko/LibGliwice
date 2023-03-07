ee8214495427f92997372fd010e91a9e27c9dad8
Grzegorz Jureczko <jureczko.grzegorz@gmail.com>
2023-03-07 09:01:53 +0100

    new book add validation and error msg

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,8 @@
 if (window.location.pathname === '/dashboard/') {
     let booksLen = document.getElementById("booksLen").value;  // takes number of positions
     for (let comp = 1; comp < booksLen; comp++) {
-        let bookAddButton = document.getElementById(`add_btn_${comp}`)
-        let bookAddComp = document.getElementById(`add_comp_${comp}`)
+        let bookAddButton = document.getElementById(`add-btn-${comp}`)
+        let bookAddComp = document.getElementById(`add-comp-${comp}`)
         bookAddButton.addEventListener('click', () => {
             if (bookAddComp.classList.contains('d-none') === false) {
                 bookAddComp.classList.toggle('d-none')
@@ -10,8 +10,8 @@ if (window.location.pathname === '/dashboard/') {
 
                 // disable all previously opened submit forms and error msg
                 for (let compDisable = 1; compDisable < booksLen; compDisable++) {
-                    let bookAddCompDisable = document.getElementById(`add_comp_${compDisable}`)
-                    let errMsgDisable = document.getElementById(`err_msg_${comp}`)
+                    let bookAddCompDisable = document.getElementById(`add-comp-${compDisable}`)
+                    let errMsgDisable = document.getElementById(`err-msg-${comp}`)
                     if (bookAddCompDisable.classList.contains('d-none') === false) {
                         bookAddCompDisable.classList.toggle('d-none')
                     }
@@ -25,19 +25,18 @@ if (window.location.pathname === '/dashboard/') {
 
         })
 
-        let submitButton = document.getElementById(`submit_button_${comp}`)
+        let submitButton = document.getElementById(`submit-button-${comp}`)
 
         // show error msg
         submitButton.addEventListener('click', e => {
-            let bookInput = document.getElementById(`input_${comp}`).value
-            let errMsg = document.getElementById(`err_msg_${comp}`)
+            let bookInput = document.getElementById(`input-${comp}`).value
+            let errMsg = document.getElementById(`err-msg-${comp}`)
             if (bookInput === "") {
-                errMsg.classList.toggle('d-none')
+                errMsg.classList.remove('d-none')
                 e.preventDefault()
             }
-
-
         })
+
 
         // let bookSegment = document.getElementById(`book_segment_${comp}`)
         // let submitButton = document.getElementById(`submit_button_${comp}`)
@@ -55,8 +54,58 @@ if (window.location.pathname === '/dashboard/') {
 if (window.location.pathname === '/collection/mylibrary') {
     let newBookFormButton = document.getElementById('new_book_form_button')
     let newBookForm = document.getElementById('new_book_form')
+    let alertMsg = document.getElementById('err-msg')
 
+    // show new book input form
     newBookFormButton.addEventListener('click', () => {
         newBookForm.classList.toggle('d-none')
+
+        // clears alerts while hiding input form
+        if (alertMsg.classList.contains('d-none') === false) {
+            alertMsg.classList.add('d-none')
+        }
     })
+
+
+    let submitButton = document.getElementById('new-book-submit-form')
+
+    submitButton.addEventListener('click', e => {
+        let titleInput = document.getElementById('input-title').value
+        let authorInput = document.getElementById('input-author').value
+        let dateInput = document.getElementById('input-date').value
+
+        let titleAlert = document.getElementById('err-title')
+        let authorAlert = document.getElementById('err-author')
+        let dateAlert = document.getElementById('err-date')
+
+        // clears alerts after new submit
+        if (titleAlert.classList.contains('d-none') === false) {
+            titleAlert.classList.add('d-none')
+        }
+        if (authorAlert.classList.contains('d-none') === false) {
+            authorAlert.classList.add('d-none')
+        }
+        if (dateAlert.classList.contains('d-none') === false) {
+            dateAlert.classList.add('d-none')
+        }
+
+        // shows proper alert after invalid submit
+        if (titleInput === "" || authorInput === "" || dateInput === "") {
+            alertMsg.classList.remove('d-none')
+            if (titleInput === "") {
+                titleAlert.classList.remove('d-none')
+            }
+            if (authorInput === "") {
+                authorAlert.classList.remove('d-none')
+            }
+            if (dateInput === "") {
+                dateAlert.classList.remove('d-none')
+            }
+            e.preventDefault()
+        }
+
+
+    })
+
+
 }
